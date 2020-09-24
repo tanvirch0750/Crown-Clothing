465d0b6bfa186c2275c1c20834468b51766223bc
tanvirch0750 <tanvir0750@yahoo.com>
2020-09-24 19:00:54 +0600

    added redux, make user reducer and user action and added to our header. and modified our app js with the help of redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
+const jsx = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
+ReactDOM.render(jsx, document.getElementById("root"));
