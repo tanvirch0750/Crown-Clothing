a3f324edb24ba580bd0e3018554a04521d316f65
tanvirch0750 <tanvir0750@yahoo.com>
2020-09-28 16:26:59 +0600

    added cart icon and cart dropdown components to our application, also implemented cart reducer

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,20 @@
+import CartActionTypes from "./cart.types";
+
+const INITIALIZE_STATE = {
+  hidden: true,
+};
+
+const cartReducer = (state = INITIALIZE_STATE, action) => {
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    default: {
+      return state;
+    }
+  }
+};
+
+export default cartReducer;
