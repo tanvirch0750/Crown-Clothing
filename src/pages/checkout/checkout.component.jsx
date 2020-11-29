cf15f3bf32ccc16cb1191a9f8799de354aec40a5
tanvirch0750 <tanvir0750@yahoo.com>
2020-11-29 22:52:14 +0600

    added stripe payment method

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartItemsCount} from '../../redux/cart/cart.selector';
 import {selectCartTotal} from '../../redux/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({cartItems, total}) => (
   <div className = 'checkout-page'>
@@ -36,6 +37,12 @@ const CheckoutPage = ({cartItems, total}) => (
         Total: ${total}
       </span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card info for example payment*
+      <br></br>
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
